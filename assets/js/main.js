7d0e316fae109f52e8402389f2b5a209d5ae2a9c
Sunny <ring2rings93@gmail.com>
2019-06-24 19:45:20 +0100

    add form and add book object to library

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,37 @@
-console.log('JS working')
+let myLibary = []
+
+function Book({title, author, pages}){
+  this.title = title
+  this.author = author
+  this.pages = pages
+}
+
+function getNodes(){
+  return{
+  $title: document.getElementById('title'),
+  $author: document.getElementById('author'),
+  $pages: document.getElementById('pages')
+  }
+}
+
+function addBook(){
+  const { $title, $author, $pages } = getNodes() 
+
+  let title = $title.value
+  let author = $author.value
+  let pages = $pages.value
+
+  let book = new Book({title, author, pages})
+  myLibary.push(book)
+  console.log(myLibary)
+  clearForm($title, $author, $pages )
+}
+
+
+function clearForm( title, author, pages){
+  title.value = ''
+  author.value = ''
+  pages.value = ''
+}
+
+
